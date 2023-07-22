5752472262664fcb2317e09d85e3992e55b77111
Afan417 <batman.two@hotmail.com>
2023-07-22 12:39:23 +0200

    created listing model, and migrations & factory seeding DB:

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,26 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            ['id' => '1', 'title' => 'Learn Laravel', 'description' => 'Learn Laravel today'],
-            ['id' => '2', 'title' => 'Learn RubyOnRails', 'description' => 'Learn RubyOnRails today'],
-            ['id' => '3', 'title' => 'Learn PHP', 'description' => 'Learn PHP today'],
-            ['id' => '4', 'title' => 'Learn Python', 'description' => 'Learn Python today'],
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
